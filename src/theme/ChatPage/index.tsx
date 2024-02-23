881495b2629539f8fa8023a76815dfe84acc5f9d
Steve Calvert <scalvert@linkedin.com>
2024-02-23 15:00:48 -0800

    Wiring up chatOptions

--- src/theme/ChatPage/index.tsx
+++ src/theme/ChatPage/index.tsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useRef } from 'react';
 import Head from '@docusaurus/Head';
 import Layout from '@theme/Layout';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 import './index.css';
 import { useTitleFormatter } from '../../utils';
+import { ThemeConfig } from '../../types';
 
 export default function ChatPage(): JSX.Element {
   const containerRef = useRef<HTMLDivElement>(null);
+  const { siteConfig } = useDocusaurusContext();
+  const {
+    glean: { chatOptions },
+  } = siteConfig.themeConfig as ThemeConfig;
 
   useEffect(() => {
     if (!window.EmbeddedSearch) return;
 
-    window.EmbeddedSearch.renderChat(containerRef.current!, {});
+    window.EmbeddedSearch.renderChat(containerRef.current!, chatOptions);
   }, [containerRef.current]);
 
   return (
